f1b37eee2b7444948f3a27758e488d73a379336c
GunnyD <45931516+DeanJG@users.noreply.github.com>
2019-05-04 20:50:23 -0700

    updated connection syntax

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,20 +1,17 @@
 const inquirer = require(`inquirer`)
 const { createConnection } = require(`mysql2`)
 
-const connection = mysql.createConnection({
+const db = createConnection({
   host: 'localhost',
-
   port: 3306,
-
   user: 'root',
-
-  password: 'root1234',
-  database: 'greatBay_DB'
+  password: 'Dg76752710!',
+  database: 'Bamazon'
 })
 
 // connect to the mysql server and sql database
-connection.connect(err => {
-  if (err) throw err
+db.connect(e => {
+  if (e) throw e
   // run the start function after the connection is made to prompt the user
   start()
 })
